8172eb8a79844d851c10a04a1a840395cf1de3b2
Gabriel Ajabahian <gajabahian@ripe.net>
2018-04-16 15:18:38 +0200

    First code commit

---
+++ Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Threading.Tasks;
+using RestSharp;
+
+namespace myAsyncApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GreetAsync().ConfigureAwait(false);
+            Console.WriteLine("Im some code in between!");
+            
+            GetGreetingsAsync1();
+            Console.WriteLine("Something after GetGreetingsAsync1!");
+            Console.ReadLine();
+        }
+        
+        private static async Task GreetAsync()
+        {
+            await Task.Run(() => GetGreetingsAsync2());
+            Console.WriteLine("Something after GetGreetingsAsync2!");
+            Console.WriteLine("Async Application Started!");
+            //Call Async method
+            Task<string> greetMsg = GetGreetingsAsync();
+
+            //Do your other stuffs synchronously while async method is in progress            
+            Console.WriteLine("Async Method in started....");
+
+            Console.WriteLine("Current Time: " + DateTime.Now);
+            Console.WriteLine("Awaiting result from Async method...");
+
+            //All work completed, wait for async method to complete
+            string msg = await greetMsg;
+
+            //Print Async Method Result
+            Console.WriteLine("Async method completed!");
+            Console.WriteLine("Current Time: " + DateTime.Now);
+            Console.WriteLine("Async method output: " + greetMsg.Result);
+
+            Console.WriteLine("Async Application Ended!");
+            Console.Read();
+        }
+        public async static Task<string> GetGreetingsAsync()
+        {
+            //simulate long running process
+            await Task.Delay(10000);
+            return "Welcome to Async C# Demo!";
+        }
+        
+        private static async void GetGreetingsAsync1()
+        {
+            //simulate long running process
+            await Task.Delay(100);
+            var client = new RestClient("https://jsonplaceholder.typicode.com");
+            var request = new RestRequest("/comments", Method.GET);
+            client.ExecuteAsync(request, response => {
+                Console.WriteLine("Response is back!");
+            });
+        }
+        
+        private static async void GetGreetingsAsync2()
+        {
+            //simulate long running process
+            await Task.Delay(100);
+            var client = new RestClient("https://jsonplaceholder.typicode.com");
+            var request = new RestRequest("/comments", Method.GET);
+            client.ExecuteAsync(request, response => {
+                Console.WriteLine("Response is back of async2!");
+            });
+        }
+    }
+}
