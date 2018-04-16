0c771d01d6a9b4da3c196b30d5de9a2bc9343269
hmallah <mallah.hicham@gmail.com>
2018-04-16 17:28:16 +0300

    adding msg2 with less delay, looking what happens

--- Program.cs
+++ Program.cs
@@ -22,7 +22,9 @@ namespace myAsyncApp
             Console.WriteLine("Something after GetGreetingsAsync2!");
             Console.WriteLine("Async Application Started!");
             //Call Async method
-            var greetMsg = GetGreetingsAsync();
+            var greetMsg = GetGreetingsAsync(10000);
+            
+            var greetMsg2 = GetGreetingsAsync(10);
 
             //Do your other stuffs synchronously while async method is in progress            
             Console.WriteLine("Async Method in started....");
@@ -31,22 +33,25 @@ namespace myAsyncApp
             Console.WriteLine("Awaiting result from Async method...");
 
             //All work completed, wait for async method to complete
+            string msg2 = await greetMsg2;
             string msg = await greetMsg;
+            
 
             //Print Async Method Result
             Console.WriteLine("Async method completed!");
             Console.WriteLine("Current Time: " + DateTime.Now);
             Console.WriteLine("Async method output: " + greetMsg.Result);
-
+            Console.WriteLine("Async method output: " + msg2);
             Console.WriteLine("Async Application Ended!");
             Console.Read();
         }
         
-        private static async Task<string> GetGreetingsAsync()
+        private static async Task<string> GetGreetingsAsync(int delay)
         {
             //simulate long running process
-            await Task.Delay(10000);
-            return "Welcome to Async C# Demo!";
+            await Task.Delay(delay);
+            Console.WriteLine(delay);
+            return delay + " Welcome to Async C# Demo!";
         }
         
         private static async void GetGreetingsAsync1()
